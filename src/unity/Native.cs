8c5687096878170478df0fee800853d2cb16686d
Kenan Schaefkofer <kbitikofer@gmail.com>
2022-01-25 15:55:40 -0500

    Add Native.cs for OvenMit <-> Unity interface

---
+++ src/unity/Native.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using System;
+using System.Runtime.InteropServices;
+
+
+namespace OvenMit
+{
+    public static class Native
+    {
+        [DllImport("OvenMit")]
+        public static extern void OvenMit_TestKeyPress();
+
+        [DllImport("OvenMit")]
+        public static extern void OvenMit_StartNote(int instance, int midiNote, int velocity);
+
+        [DllImport("OvenMit")]
+        public static extern void OvenMit_ReleaseNote(int instance, int midiNote);
+
+        [DllImport("OvenMit")]
+        public static extern void OvenMit_StopAllNotes(int instance);
+
+        [DllImport("OvenMit")]
+        public static extern void OvenMit_StopAllSounds(int instance);
+
+        [DllImport("OvenMit")]
+        public static extern long OvenMit_GetTimeInSamples();
+
+    }
+}
