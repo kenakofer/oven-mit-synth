4ddfee93469500fea1027e2a47145eeab1a2f5b6
Kenan Schaefkofer <kbitikofer@gmail.com>
2022-01-26 16:19:53 -0500

    Add some Unity patch loading from LMMS preset xml
    Just printing the loaded values right now.

---
+++ builds/unity/Scripts/Param.cs
@@ -0,0 +1,64 @@
+namespace OvenMit
+{
+    public static class Param
+    {
+        public enum SynthParameters {
+            P_ENV_MODE_1,
+            P_ATTACK    ,
+            P_DECAY     ,
+            P_SUSTAIN   ,
+            P_RELEASE   ,
+
+            P_WAVEFORM  ,
+            P_LEVEL     ,
+            P_PITCH     ,
+
+            P_FILTER    ,
+            P_CUTOFF    ,
+            P_RES_HEIGHT,
+            P_RES_WIDTH ,
+
+            P_ENV_MODE_2,
+            P_ENV_AMT_2 ,
+            P_ATTACK_2  ,
+            P_DECAY_2   ,
+            P_SUSTAIN_2 ,
+            P_RELEASE_2 ,
+
+            P_WAVEFORM_2_MODE,
+            P_WAVEFORM_2,
+            P_LEVEL_2,
+            P_PITCH_2,
+
+            P_VOICE_MODE,
+            P_PORTAMENTO
+        };
+
+        public static readonly string[] PARAMETER_SYMBOLS = {
+            "env_mode_1",
+            "attack",
+            "decay",
+            "sustain",
+            "release",
+            "waveform",
+            "level",
+            "pitch1",
+            "filter",
+            "cutoff",
+            "res_height",
+            "res_width",
+            "env_mode_2",
+            "env_amount_2",
+            "attack2",
+            "decay2",
+            "sustain2",
+            "release2",
+            "waveform2mode",
+            "waveform2",
+            "level2",
+            "pitch2",
+            "voice_mode",
+            "portamento"
+        };
+    }
+}
---
+++ builds/unity/Scripts/PatchLoader.cs
@@ -0,0 +1,64 @@
+using UnityEngine;
+using System;
+using System.Runtime.InteropServices;
+using System.Xml;
+
+
+namespace OvenMit
+{
+    public static class PatchLoader
+    {
+        const string LMMS_XML_PARAMETER_TAG_NAME = "models";
+
+        public static readonly Param.SynthParameters[] LMMS_ATTRIBUTE_PARAMETERS= {
+            Param.SynthParameters.P_ENV_MODE_1,
+            Param.SynthParameters.P_SUSTAIN   ,
+            Param.SynthParameters.P_WAVEFORM  ,
+            Param.SynthParameters.P_LEVEL     ,
+            Param.SynthParameters.P_PITCH     ,
+            Param.SynthParameters.P_FILTER    ,
+            Param.SynthParameters.P_CUTOFF    ,
+            Param.SynthParameters.P_RES_HEIGHT,
+            Param.SynthParameters.P_RES_WIDTH ,
+            Param.SynthParameters.P_ENV_MODE_2,
+            Param.SynthParameters.P_ENV_AMT_2 ,
+            Param.SynthParameters.P_SUSTAIN_2 ,
+            Param.SynthParameters.P_WAVEFORM_2_MODE,
+            Param.SynthParameters.P_WAVEFORM_2,
+            Param.SynthParameters.P_LEVEL_2,
+            Param.SynthParameters.P_PITCH_2,
+            Param.SynthParameters.P_VOICE_MODE
+        };
+
+        // When the scale_type is log, LMMS stores those parameters as child elements
+        public static readonly Param.SynthParameters[] LMMS_CHILD_PARAMETERS= {
+            Param.SynthParameters.P_ATTACK    ,
+            Param.SynthParameters.P_DECAY     ,
+            Param.SynthParameters.P_RELEASE   ,
+            Param.SynthParameters.P_ATTACK_2  ,
+            Param.SynthParameters.P_DECAY_2   ,
+            Param.SynthParameters.P_RELEASE_2 ,
+            Param.SynthParameters.P_PORTAMENTO
+        };
+
+        public static void LoadPatchFromFile(string path)
+        {
+            string fullPath = Application.streamingAssetsPath + "/" + path;
+            Debug.Log($"Application.streamingAssetsPath is {Application.streamingAssetsPath}");
+            XmlDocument xmlDoc = new XmlDocument();
+            xmlDoc.Load(fullPath);
+            foreach (XmlElement elem in xmlDoc.GetElementsByTagName("models")) {
+                foreach (Param.SynthParameters param in LMMS_ATTRIBUTE_PARAMETERS) {
+                    string name = Param.PARAMETER_SYMBOLS[(int)param];
+                    float value = float.Parse(elem.GetAttribute(name));
+                    Debug.Log("Attribute: "+ (int)param + " " + name + " " + value.ToString());
+                }
+                foreach (Param.SynthParameters param in LMMS_CHILD_PARAMETERS) {
+                    string name = Param.PARAMETER_SYMBOLS[(int)param];
+                    float value = float.Parse(elem[name].GetAttribute("value"));
+                    Debug.Log("Attribute: "+ (int)param + " " + name + " " + value.ToString());
+                }
+            }
+        }
+    }
+}
