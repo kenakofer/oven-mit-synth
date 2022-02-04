5dfc66a35ae855bfa9f00f321493bc6267e90bc7
Kenan Schaefkofer <kbitikofer@gmail.com>
2022-02-04 10:16:21 -0500

    Allow pitch2 to go down to -48

--- src/Param.hpp
+++ src/Param.hpp
@@ -65,7 +65,7 @@ constexpr std::array<std::pair<float, float>, P_NUM_CONTROLS> PARAM_LIMIT =
     {0.0f, 3.0f},           // WAVEFORM 2 MODE
     {0.0f, 4.0f},           // WAVEFORM 2
     {0.0f, 1.0f},           // LEVEL 2
-    {-24.0f, 24.0f},        // PITCH 2
+    {-48.0f, 24.0f},        // PITCH 2
 
     {0.0f, 2.0f},           // VOICE MODE
     {1.00f, 100.0f},        // PORTAMENTO
