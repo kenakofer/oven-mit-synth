310e4ca60aa9368abe24f4a7619391076ea65b45
Kenan Schaefkofer <kbitikofer@gmail.com>
2022-02-05 08:51:41 -0500

    Add helpful comments to debug script

--- src/scripts/makeLV2Debug.sh
+++ src/scripts/makeLV2Debug.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
  g++ -fvisibility=hidden -O3 -ffast-math -fPIC -Wl,-Bstatic -Wl,-Bdynamic -Wl,--as-needed -shared -pthread `pkg-config --cflags lv2` -lm `pkg-config --libs lv2` -g src/lv2/lv2OvenMit.cpp -o builds/lv2/linux64/OvenMit.so
+
+
+## To test with jalv, for example:
+# src/scripts/makeLV2Debug.sh && gdb jalv.gtk3
+## Then type
+# run https://github.com/kenakofer/oven-mit-synth
+
+# If you do this, note that using Ctrl-c to interrupt will interrupt the gtk
+# thread. If you want to interrupt the oven mit logic thread, you'll need to
+# find its PID, then `kill -INT` it.
