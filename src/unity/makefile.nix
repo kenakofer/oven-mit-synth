26b26a5acc85d033da79aa3aba22e5de8854b797
Kenan Schaefkofer <kbitikofer@gmail.com>
2022-02-12 18:10:41 -0500

    Mess with 'nix makefile g++ flags

--- src/unity/makefile.nix
+++ src/unity/makefile.nix
@@ -2,9 +2,15 @@ SRCS=AudioPluginUtil.cpp \
 Plugin_OvenMit.cpp
 OBJS=$(SRCS:.cpp=.o)
 OUTPUT=../../builds/unity/Plugins/Linux64/libOvenMit.so
-CXXFLAGS=-I. -O2 -fPIC
+# Works
+# CXXFLAGS=-I. -O2 -fPIC
+# CXXFLAGS=-g -I. -O3 -ffast-math -fPIC
+CXXFLAGS=-g -I. -O3 -ffast-math -fPIC -Wl,-Bstatic -Wl,-Bdynamic -Wl,--as-needed
+
+# Doesn't work
+# CXXFLAGS=-g -fvisibility=hidden -I. -O3 -ffast-math -fPIC -Wl,-Bstatic -Wl,-Bdynamic -Wl,--as-needed
+
 LDFLAGS=-shared -rdynamic -fPIC
-# CXXFLAGS=-fvisibility=hidden -I. -O3 -ffast-math -fPIC -Wl,-Bstatic -Wl,-Bdynamic -Wl,--as-needed
 # LDFLAGS=-shared -rdynamic -fPIC
 CXX=g++
 
